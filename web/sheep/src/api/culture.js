2ec63846c497815e492dbab2bdb5d427bf2da209
yuanhaiyang <yuanhaiyang@eatown.com.cn>
2019-12-29 21:43:56 +0800

    feat: new culture wall api

---
+++ web/sheep/src/api/culture.js
@@ -0,0 +1,81 @@
+import axios from 'axios'
+
+export const getCulturePictures = data => {
+  let pageNumber = data.pageNumber
+  let pictures = []
+  if (data.page > 3) {
+    return pictures
+  }
+  return [
+    {
+      time: '2019-12-19',
+      pictures: [
+        {
+          name: 'picture-1',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          time: '2019-12-19'
+        },
+        {
+          name: 'picture-2',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-19'
+        },
+        {
+          name: 'picture-3',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-19'
+        }
+      ]
+    },
+    {
+      time: '2019-12-17',
+      pictures: [
+        {
+          name: 'picture-4',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-17'
+        },
+        {
+          name: 'picture-5',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-17'
+        }
+      ]
+    },
+    {
+      time: '2019-12-16',
+      pictures: [
+        {
+          name: 'picture-6',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-16'
+        },
+        {
+          name: 'picture-7',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-16'
+        },
+        {
+          name: 'picture-8',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-16'
+        },
+        {
+          name: 'picture-9',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-16'
+        },
+        {
+          name: 'picture-10',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-16'
+        },
+        {
+          name: 'picture-11',
+          src: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+          price: '2019-12-16'
+        }
+      ]
+    }
+  ]
+}
